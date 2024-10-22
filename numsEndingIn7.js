3557edcd600106f6d9efcc6c2a5ba4c99a80d828
Andrey Dimitrov <andrey89dimitrov@gmail.com>
2024-10-22 14:44:59 +0300

    Add files via upload

---
+++ numsEndingIn7.js
@@ -0,0 +1,9 @@
+function numsEndingIn7() {
+for (currentNum = 1; currentNum <= 1000; currentNum++){
+    if(currentNum % 10 === 7) {
+        console.log(currentNum);
+        
+    }
+}
+}
+numsEndingIn7();
